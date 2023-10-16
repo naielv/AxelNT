2f752150151ca31d07ee305104c9a8e4167e29dd
naielv <109038805+naielv@users.noreply.github.com>
2023-10-16 14:58:11 +0200

    update

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 cp -r predist dist
 cp -r src/ dist/boot/python/axelnt/
 
+sudo apt install syslinux-utils -y
+
 xorriso \
   -outdev ./axelnt.iso -blank as_needed \
   -joliet on \
   -map dist / \
   -volid "AXELNT"
-
+sudo isohybrid ./axelnt.iso
 rm -rf dist/
